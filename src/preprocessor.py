758c1b048db11b02c462b8ded3b3c45f5656c9c0
Deepanshu Gupta <41968942+deepanshug4@users.noreply.github.com>
2022-04-03 19:29:17 +0530

    Update preprocessor.py

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -12,8 +12,7 @@ class Preprocessor:
                  img_size: Tuple[int, int],
                  padding: int = 0,
                  dynamic_width: bool = False,
-                 data_augmentation: bool = False,
-                 line_mode: bool = False) -> None:
+                 data_augmentation: bool = False) -> None:
         # dynamic width only supported when no data augmentation happens
         assert not (dynamic_width and data_augmentation)
         # when padding is on, we need dynamic width enabled
@@ -23,7 +22,6 @@ class Preprocessor:
         self.padding = padding
         self.dynamic_width = dynamic_width
         self.data_augmentation = data_augmentation
-        self.line_mode = line_mode
 
     @staticmethod
     def _truncate_label(text: str, max_text_len: int) -> str:
@@ -42,51 +40,6 @@ class Preprocessor:
                 return text[:i]
         return text
 
-    def _simulate_text_line(self, batch: Batch) -> Batch:
-        """Create image of a text line by pasting multiple word images into an image."""
-
-        default_word_sep = 30
-        default_num_words = 5
-
-        # go over all batch elements
-        res_imgs = []
-        res_gt_texts = []
-        for i in range(batch.batch_size):
-            # number of words to put into current line
-            num_words = random.randint(1, 8) if self.data_augmentation else default_num_words
-
-            # concat ground truth texts
-            curr_gt = ' '.join([batch.gt_texts[(i + j) % batch.batch_size] for j in range(num_words)])
-            res_gt_texts.append(curr_gt)
-
-            # put selected word images into list, compute target image size
-            sel_imgs = []
-            word_seps = [0]
-            h = 0
-            w = 0
-            for j in range(num_words):
-                curr_sel_img = batch.imgs[(i + j) % batch.batch_size]
-                curr_word_sep = random.randint(20, 50) if self.data_augmentation else default_word_sep
-                h = max(h, curr_sel_img.shape[0])
-                w += curr_sel_img.shape[1]
-                sel_imgs.append(curr_sel_img)
-                if j + 1 < num_words:
-                    w += curr_word_sep
-                    word_seps.append(curr_word_sep)
-
-            # put all selected word images into target image
-            target = np.ones([h, w], np.uint8) * 255
-            x = 0
-            for curr_sel_img, curr_word_sep in zip(sel_imgs, word_seps):
-                x += curr_word_sep
-                y = (h - curr_sel_img.shape[0]) // 2
-                target[y:y + curr_sel_img.shape[0]:, x:x + curr_sel_img.shape[1]] = curr_sel_img
-                x += curr_sel_img.shape[1]
-
-            # put image of line into result
-            res_imgs.append(target)
-
-        return Batch(res_imgs, res_gt_texts, batch.batch_size)
 
     def process_img(self, img: np.ndarray) -> np.ndarray:
         """Resize to target size, apply data augmentation."""
@@ -166,9 +119,7 @@ class Preprocessor:
         return img
 
     def process_batch(self, batch: Batch) -> Batch:
-        if self.line_mode:
-            batch = self._simulate_text_line(batch)
-
+        
         res_imgs = [self.process_img(img) for img in batch.imgs]
         max_text_len = res_imgs[0].shape[0] // 4
         res_gt_texts = [self._truncate_label(gt_text, max_text_len) for gt_text in batch.gt_texts]
