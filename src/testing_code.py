be46fb1c2daf30552e768821a043c5871a584d01
Deepanshu Gupta <41968942+deepanshug4@users.noreply.github.com>
2022-04-09 02:39:52 +0530

    Contains the Infer method used for testing.

--- src/testing_code.py
+++ src/testing_code.py
@@ -15,14 +15,12 @@ class FilePaths:
     fn_corpus = '../data/corpus.txt'
 
 
-def get_img_height() -> int:
-    """Fixed height for NN."""
-    return 32
-
-
-def get_img_size() -> Tuple[int, int]:
+def get_img_size(type_of_model) -> Tuple[int, int]:
     """Height is fixed for NN, width is set according to training mode (single words)."""
-    return 128, get_img_height()
+    if type_of_model:
+        return 256,32
+    else:
+        return 128, 32
 
 
 def char_list_from_file() -> List[str]:
@@ -30,16 +28,16 @@ def char_list_from_file() -> List[str]:
         return list(f.read())
 
 
-def infer(model: Model, fn_img: Path) -> None:
+def infer(model: Model, fn_img: Path, type_of_model) -> None:
     """Recognizes text in image provided by file path."""
     img = cv2.imread(fn_img, cv2.IMREAD_GRAYSCALE)
     assert img is not None
 
-    preprocessor = Preprocessor(get_img_size(), dynamic_width=True, padding=16)
+    preprocessor = Preprocessor(get_img_size(type_of_model), dynamic_width=True, padding=16,model_mode=type_of_model)
     img = preprocessor.process_img(img)
 
     batch = Batch([img], None, 1)
-    recognized, probability = model.infer_batch(batch, True)
+    recognized, probability = model.infer_batch(batch, True, 1)
     print(f'Recognized: "{recognized[0]}"')
     print(f'Probability: {probability[0]}')
     return recognized[0], probability[0]
